5290c001e507e13f4f9bd61e7906bf2a73292be1
Riteeka <riteekagawand7@gmail.com>
2024-07-13 22:43:02 +0530

    Largest Number

---
+++ Basics/swap2num.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class swap2num {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter Second Number: ");
+        int num2 = sc.nextInt();
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("First Number: "+num1);
+        System.out.println("Second Number: "+num2);
+    }
+}
